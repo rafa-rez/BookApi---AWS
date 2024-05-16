481f1ea972898d6775ae8f960e07bbafa5b6c8da
Guilherme Padilha Jacon <guilhermepjacon@gmail.com>
2024-05-15 21:16:24 -0300

    feat: estrutura básica da rota reviews

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -16,6 +16,24 @@ async function overview(){
     const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${process.env.API_KEY}`)
     const data = await response.json()
     return data
-}
+} 
 
-module.exports = { bestSellers, names, overview }
+// Função da rota "/reviews" desenvolvida por: Guilherme 
+async function reviews() { 
+    const author = 'Stephen King';
+    const encodedAuthor = encodeURIComponent(author);
+    try {
+        const response = await fetch(`https://api.nytimes.com/svc/books/v3/reviews.json?author=${encodedAuthor}&api-key=${process.env.API_KEY}`);
+        if (!response.ok) {
+            throw new Error('Erro ao obter resenhas dos livros');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao obter resenhas dos livros:', error.message);
+        return { error: error.message };
+    }
+}
+ 
+
+module.exports = { bestSellers, names, overview, reviews }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -39,7 +39,18 @@ router.get('/overview', async (req, res) => {
         console.error(error)
         res.status(500).json({ error: 'Internal Server Error' })
     }
+}) 
+
+// - Guilherme 
+router.get('/reviews', async (req, res) => {
+    try {
+        const data = await booksController.reviews()
+        res.json(data)
+        console.log("deu certo - reviews")
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
 
 module.exports = router
-
